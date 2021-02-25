ebabdb239d32a42d77fe68a84cc5fc3cb2f2d3d5
MarcoMoran971 <78183654+MarcoMoran971@users.noreply.github.com>
2021-02-24 18:33:03 -0600

    Create Blog.js

---
+++ Blog.js
@@ -0,0 +1,36 @@
+//Creacion del arreglo
+let posts = [
+    {
+    autor: 'Marco',
+    text: 'Este es el primer Blog.',
+    fecha: 31/01/2021,
+    categoria: ['general', 'F1'],
+    },
+    {
+    autor: 'Karla',
+    text: 'Este es el segundo blog.',
+    fecha: 31/01/2021,
+    categoria: ['general', 'Donde habitan los angeles'],
+    },
+]
+//Creacion de post
+const addPost = params => {
+    const { posts=[], autor, text, fecha, categoria} = params
+    //post
+    
+    //autor
+
+
+    ///text
+
+
+    //fecha
+    let date = new Date();
+     fecha: date;
+
+    //Categoria
+    const categoria = ({post, categoria, categoria1,categoria2}) =>{
+    post[categoria].push(categoria1,categoria2);    
+    }
+    return posts
+}
